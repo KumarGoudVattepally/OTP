39292d7bd7ae8dc7176d1ccb5a9af6ab2dd81526
Kumar Goud Vattepally <kumargoudvattepally3@gmail.com>
2024-11-22 23:04:50 +0530

    otp

---
+++ index.js
@@ -0,0 +1,17 @@
+
+// let form=document.getElementById("form")
+// let text=document.getElementById("text")
+// //     const  evaluation =(event)=>{
+// // event.preventDefault();
+// // let res= evaluation(text.value)
+// //    text.value=res
+// //     }
+// let a 
+// let c
+// function ran() {
+//     a=(Math.trunc(Math.random()*999+1000))
+//     text.value=a
+//    b=(Math.floor(Math.random()*99999+100000)) 
+//   c= document.body.style.backgroundColor = "#b";
+  
+// }
